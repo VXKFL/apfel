07c681ba368d23f754e18e78a54bbf912a0daead
VXKFL <remo@stitronic.de>
2021-10-19 16:43:45 +0200

    Redirect to register page if no code is found

--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,8 @@
+if (!window.localStorage.getItem("Code")) {
+	window.alert("Du bist noch nicht registriert.")
+	window.location.replace("/register")
+}
+
 const code = window.localStorage.getItem("Code");
 document.getElementById("Code").innerText = code;
 
